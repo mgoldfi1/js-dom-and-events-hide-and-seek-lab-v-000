a23223f48fe35f0dd0310d2dca631058eb083baa
Matthew Goldfine <mgoldfi1@binghamton.edu>
2018-09-10 17:17:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function nestedTarget() {
 }
 
 function deepestChild() {
-  return document.querySelector('#grand-node')
+  return document.querySelector('#grand-node div div div div')
 }
 
 function increaseRankBy(n) {
