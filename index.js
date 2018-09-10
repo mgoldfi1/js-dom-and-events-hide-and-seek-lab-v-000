d4301587a9db571b66d88dc7c33f6d95d06d6aa9
Matthew Goldfine <mgoldfi1@binghamton.edu>
2018-09-10 17:19:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,9 +11,5 @@ function deepestChild() {
 }
 
 function increaseRankBy(n) {
-  const lis = document.getElementById('app').querySelectorAll('ul.ranked-list li')
 
-  for (let i = 0; i < lis.length; i++) {
-  lis[i].innerHTML = (i + 1).toString()
-}
 }
