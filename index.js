fd0ea0525b1cfc93b8fa6c498dc688aa13ab2a38
Matthew Goldfine <mgoldfi1@binghamton.edu>
2018-09-10 17:10:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ return document.querySelector(selector)
 }
 
 function nestedTarget() {
-  return document.getElementById(nested).querySelectorAll(target)
+  return document.getElementById(nested)
 }
