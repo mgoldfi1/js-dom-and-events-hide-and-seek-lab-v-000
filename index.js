1cae8f0527e74dceea10211a899024a690be49a9
Matthew Goldfine <mgoldfi1@binghamton.edu>
2018-09-10 17:30:46 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,17 @@
+function getFirstSelector(selector) {
+return document.querySelector(selector)
+}
+
+function nestedTarget() {
+  return document.getElementById('nested')
+}
+
+function deepestChild() {
+  return document.querySelector('#grand-node div div div div')
+}
+
+function increaseRankBy(n) {
+ for (const x of document.querySelectorAll('.ranked-list li')) {
+   x.innerHTML = parseInt(x.innerHTML) + n
+ }
+}
